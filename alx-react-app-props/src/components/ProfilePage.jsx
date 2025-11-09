69da9d938080c7e2872eef87155040d1bd59ac25
OKEWU SAMUEL OWOICHO <okewusamuelowoicho@gmail.com>
2025-11-09 02:53:32 +0100

    Update ProfilePage.jsx

--- alx-react-app-props/src/components/ProfilePage.jsx
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import UserInfo from './components/UserInfo';
 
 function ProfilePage({ useContext }) {
-  return <UserInfo userData={userContext} />;
+  return <UserInfo userData={UserContext} />;
 }
 
 export default ProfilePage;
