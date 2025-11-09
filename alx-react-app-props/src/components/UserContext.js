abc0f8cb7cc00231d0092df3be6db134a2a74c50
OKEWU SAMUEL OWOICHO <okewusamuelowoicho@gmail.com>
2025-11-09 03:27:40 +0100

    Delete App.jsx

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,6 +1,10 @@
 import {createcontext} from 'react';
 
+<<<<<<< HEAD
 export const Context = React.createContext();
+=======
+export const UserContext = React.createContext();
+>>>>>>> parent of 77e84a0 (Update UserContext.js)
 
 export default UserContext;
 
