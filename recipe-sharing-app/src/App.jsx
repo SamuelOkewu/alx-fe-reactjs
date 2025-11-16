ad4da63c0bf00632b5c2a134e714fb9c0fd62783
OKEWU SAMUEL OWOICHO <okewusamuelowoicho@gmail.com>
2025-11-16 20:43:42 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import store from './store.jsx'
+import Recipelist from '.RecipeList.jsx'
+import { useCounterStore } from './store.jsx'
+import AddRecipeform from '.AddRecipeForm.jsx'
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const count = useCounterStore((state) => state.count)
 
   return (
     <>
