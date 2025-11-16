f572c1e70541506dd8ade043d890345b3772bf4f
OKEWU SAMUEL OWOICHO <okewusamuelowoicho@gmail.com>
2025-11-16 22:30:50 +0100

    Update DeleteRecipeButton.jsx

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -2,7 +2,7 @@ import { useRecipeStore } from './recipeStore';
 
 const DeleteRecipeButton = ({ id }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
-
+useNavigate
   return (
     <button onClick={() => deleteRecipe(id)}>
       Delete Recipe
